cf37f833ad6a300dc25f4845b3e34eabd64f79d4
Antonio <Antonirrox@gmail.com>
2019-08-07 12:32:01 -0400

    first try to set an image as background

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="app" class="bg-gray-100 h-screen">
+    <div id="app" class="bg-fixed" style="background-image: url(https://previews.123rf.com/images/sanek13744/sanek137441712/sanek13744171200457/92330417-para-hacer-una-lista-de-fondo-sin-patr%C3%B3n-ilustraci%C3%B3n-de-vector-plano-de-negocios-lista-de-verificaci%C3%B3n-patr%C3%B3n-de.jpg)">
         <TodoList/>
     </div>
 </template>
